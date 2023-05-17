e0dbc5b19712e79c8365fa2ccd5711d95dfcc2fe
Menonashwin <menonashwin2002@gmail.com>
2023-05-17 12:28:38 +0530

    commit

---
+++ bin/localvariable.dart
@@ -0,0 +1,13 @@
+class operation{
+  int a = 10;
+  int b = 20;
+
+  void add(){
+    int c = 50;
+    print(a+b+c);
+  }
+}
+void main(){
+  operation obj = operation();
+  obj.add();
+}
---
+++ bin/operators.dart
@@ -0,0 +1,9 @@
+void main(){
+  var a = 120, b = 56;
+  print("sum = ${a+b}");
+  print("sub = ${a-b}");
+  print("mul = ${a*b}");
+  print("div = ${a/b}");
+  print("modu= ${a%b}");
+  print("tilt = ${a~/b}");
+}
