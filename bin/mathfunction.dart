f20fe2ed02287f00e695cbb9a49fac74e6930101
Menonashwin <menonashwin2002@gmail.com>
2023-05-17 12:17:33 +0530

    commit

---
+++ bin/mathfunction.dart
@@ -0,0 +1,23 @@
+void add(){
+  int a =15, b=20;
+  print("sum = ${a+b}");
+}
+
+void sub(){
+  int a = 20, b = 5;
+  print("substract = ${a-b}");
+}
+void div(){
+  double a = 125, b =20;
+  print("divisiom = ${a/b}");
+}
+void mult() {
+  int a = 20, b = 2;
+  print("multiplication = ${a * b}");
+}
+void main(){
+  add();
+  sub();
+  div();
+  mult();
+}
